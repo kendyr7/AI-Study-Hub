68e619c341e2416ae334246712e0bf3df9a59beb
Kendyr Quintanilla <kquintanilla@alonsoalonsolaw.com>
2025-07-09 05:55:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,10 @@ const nextConfig: NextConfig = {
         ...config.resolve.fallback,
         async_hooks: false,
         fs: false,
+        tls: false,
+        net: false,
+        dns: false,
+        http2: false,
       };
     }
     return config;
